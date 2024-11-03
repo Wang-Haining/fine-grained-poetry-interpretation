c50c675ee279ad46b43d5afa310bd49d732a3763
hw@chill <hw56@indiana.edu>
2024-11-03 17:05:45 -0500

    feat: add sft draft

---
+++ sft.py
@@ -0,0 +1,146 @@
+"""
+This module performs supervised fine-tuning of LLMs using:
+
+- GPT-4 generated interpretations: the Fine-grained Poetry Interpretation corpus for
+the Poetry Foundation Corpus and Public Domain Poetry Corpus.
+- AllPoetry Corpus: filtered users' comments on their feelings and interpretations of
+poetry.
+
+Currently, we only support two models:
+- 'allenai/OLMo-1B-0724-hf' (with a context length of 4096)
+- 'google/gemma-2-2b' (with a context length of 8192)
+
+"""
+
+__author__ = "hw56@indiana.edu"
+__version__ = "0.0.1"
+__license__ = "0BSD"
+
+import argparse
+import os
+from typing import List
+
+import torch
+import wandb
+from datasets import DatasetDict, load_from_disk, concatenate_datasets
+from transformers import (AutoModelForCausalLM, AutoTokenizer,
+                          EarlyStoppingCallback, TrainingArguments)
+from trl import SFTTrainer, set_seed
+
+from utils import (CKPTS_DIR, DATASET_PATH, MAX_INPUT_LENGTHS, MAX_OUTPUT_LENGTHS,
+                   PROJECT_NAME, RESPONSE_TEMP, TASK_PREFIX)
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+CKPTS_DIR = 'ckpts'
+# todo: you have to have a pre-fixed split
+POETRY_FOUDATION_INTERPRETATION_CORPUS = 'interpretation/poetry_foundation.parquet'
+PUBLIC_DOMAIN_POETRY_INTERPRETATION_CORPUS = 'public_domain_poetry.parquet'
+ALL_POETRY_CORPUS = ''
+
+
+def formatting_func(example: DatasetDict) -> List[str]:
+    """
+    Formats input examples by concatenating the source text with the target text,
+    using the task-specific prefix and response template.
+
+    Args:
+        example: A dataset dictionary containing 'source' and 'target' fields.
+
+    Returns:
+        A list of formatted strings ready for model training.
+    """
+    output_texts = []
+    for i in range(len(example["source"])):
+        text = (
+            TASK_PREFIX
+            + f"{example['source'][i]}{RESPONSE_TEMP} {example['target'][i]}"
+        )
+        output_texts.append(text)
+
+    return output_texts
+
+
+if __name__ == "__main__":
+
+    set_seed(42)
+    parser = argparse.ArgumentParser(description="Supervise Fine-tuning OLMo-1B (0724) or Gemma-2-2B")
+    parser.add_argument("--model", type=str,
+                        choices=['allenai/OLMo-1B-0724-hf', 'google/gemma-2-2b'],
+                        help="A huggingface model's name")
+    parser.add_argument("--corpus", type=str,
+                        choices=['poetry_foundation', 'public_domain_poetry', 'all_poetry'],
+                        nargs='+', help="One or more corpora to use; if multiple are selected, "
+                        "they will be mixed proportionally based on their sizes "
+                        "(i.e., proportional random sampling)")
+    parser.add_argument("--learning_rate", type=float, default=1e-5)
+    parser.add_argument("--per_device_train_batch_size",
+                        type=int, default=2)
+    parser.add_argument("--gradient_checkpointing", action='store_true',
+                        help="Whether to use gradient checkpointing")
+    parser.add_argument("--deepspeed", action='store_true',
+                        help="Whether to use DeepSpeed for training")
+    args = parser.parse_args()
+
+    run_name = f'sft_{args.model.split("/")[-1]}'
+
+    training_args = TrainingArguments(
+        output_dir=f"{CKPTS_DIR}/{run_name}",
+        overwrite_output_dir=True,
+        num_train_epochs=50.0,
+        do_train=True,
+        do_eval=True,
+        eval_strategy="steps",
+        per_device_train_batch_size=args.per_device_train_batch_size,
+        per_device_eval_batch_size=args.per_device_train_batch_size,  # same to training
+        gradient_accumulation_steps=4,
+        learning_rate=args.learning_rate,
+        lr_scheduler_type='constant_with_warmup',
+        warmup_steps=50,
+        weight_decay=1e-1,
+        logging_steps=100,
+        eval_steps=100,
+        bf16=True,
+        report_to="wandb",
+        load_best_model_at_end=True,
+        save_steps=100,
+        save_total_limit=3,
+        remove_unused_columns=True,
+        gradient_checkpointing=args.gradient_checkpointing,
+        gradient_checkpointing_kwargs={'use_reentrant': False} if args.gradient_checkpointing else None,
+        deepspeed='runs/ds_sft_config.json' if args.deepspeed else None,
+    )
+    wandb.init(project=PROJECT_NAME, name=run_name, config=training_args.to_dict())
+
+    tokenizer = AutoTokenizer.from_pretrained(args.model, padding_side="right")
+    # init dataset
+    datasets = []
+    if 'poetry_foundation' in args.corpus:
+        pf_ds = load_from_disk(POETRY_FOUDATION_INTERPRETATION_CORPUS)
+        datasets.append(pf_ds)
+    if 'public_domain_poetry' in args.corpus:
+        pdp_ds = load_from_disk(PUBLIC_DOMAIN_POETRY_INTERPRETATION_CORPUS)
+        datasets.append(pdp_ds)
+    if 'all_poetry' in args.corpus:
+        ap_ds = load_from_disk(ALL_POETRY_CORPUS)
+        datasets.append(ap_ds)
+    # combine and shuffle
+    if datasets:
+        dataset = concatenate_datasets(datasets)
+        dataset = dataset.shuffle(seed=42)
+
+    # init model after trainingArgs init
+    model = AutoModelForCausalLM.from_pretrained(args.model,
+                                                 torch_dtype=torch.bfloat16)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=args.learning_rate)
+
+    trainer = SFTTrainer(
+        model=model,
+        train_dataset=dataset["train"],
+        eval_dataset=dataset["validation"],
+        formatting_func=formatting_func,
+        max_seq_length=MAX_INPUT_LENGTHS[args.model] + MAX_OUTPUT_LENGTHS[args.model] + 10,
+        args=training_args,
+        callbacks=[EarlyStoppingCallback(early_stopping_patience=5)],
+    )
+
+    trainer.train()
