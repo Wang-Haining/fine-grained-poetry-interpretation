b27bb8d32c1da3f162e8e80a2db62bd9362a9d2f
hw@chill <hw56@indiana.edu>
2024-11-03 12:09:28 -0500

    fix: coverting back to df before train_test_split

--- create_interpretation_corpus.py
+++ create_interpretation_corpus.py
@@ -1,14 +1,14 @@
-from datasets import load_from_disk, concatenate_datasets, Dataset, DatasetDict
-from sklearn.model_selection import train_test_split
+from datasets import Dataset, DatasetDict
 import pandas as pd
 from nltk.tokenize import casual_tokenize
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 POETRY_INTERPRETATION_CORPUS = 'interpretation/poetry_interpretation_corpus_v001.hf'
 
 # load datasets
-pf_ds = pd.read_parquet('interpretation/poetry_foundation.parquet')
-pdp_ds = pd.read_parquet('interpretation/public_domain_poetry.parquet')
+pf_ds = pd.read_parquet('interpretation/poetry_foundation.parquet')  # 13,854
+pdp_ds = pd.read_parquet('interpretation/public_domain_poetry.parquet')  # 38,499
 pf_ds = Dataset.from_pandas(pf_ds)
 pdp_ds = Dataset.from_pandas(pdp_ds)
 
@@ -21,12 +21,16 @@ dataset = concatenate_datasets([pf_ds, pdp_ds])
 
 # remove duplicates, based on author and title
 dataset = dataset.to_pandas().drop_duplicates(subset=['author', 'title'])
-dataset = Dataset.from_pandas(dataset)
 print(f'The raw combined dataset has {dataset.shape[0]} entries before duplication.')
 
-# shuffle and split
+# shuffle and split using train_test_split
 train_data, val_test_data = train_test_split(dataset, test_size=0.1, random_state=42)
 valid_data, test_data = train_test_split(val_test_data, test_size=0.5, random_state=42)
+
+# convert back to Dataset
+train_data = Dataset.from_pandas(train_data)
+valid_data = Dataset.from_pandas(valid_data)
+test_data = Dataset.from_pandas(test_data)
 
 # create a DatasetDict
 dataset_dict = DatasetDict({
@@ -38,17 +42,16 @@ dataset_dict = DatasetDict({
 # save
 dataset_dict.save_to_disk(POETRY_INTERPRETATION_CORPUS)
 
-
 # corpus statistics
 corpus_stats = {
-    'number_of_entries': dataset.to_pandas().groupby('source').size().to_dict(),
-    'number_of_authors': dataset.to_pandas()['author'].nunique(),
+    'number_of_entries': dataset.groupby('source').size().to_dict(),
+    'number_of_authors': dataset['author'].nunique(),
     'percentile_length_distribution': {}
 }
 
 # length distributions
-poem_lengths = dataset.map(lambda x: {'poem_length': len(casual_tokenize(x['poem']))})['poem_length']
-interpretation_lengths = dataset.map(lambda x: {'interpretation_length': len(casual_tokenize(x['interpretation']))})['interpretation_length']
+poem_lengths = dataset.apply(lambda x: len(casual_tokenize(x['poem'])), axis=1)
+interpretation_lengths = dataset.apply(lambda x: len(casual_tokenize(x['interpretation'])), axis=1)
 
 # calculate percentiles
 poem_percentiles = np.percentile(poem_lengths, [25, 50, 75])
